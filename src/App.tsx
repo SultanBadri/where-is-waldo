f7a2509f002ad11d1b77aeb3bccc36962160e7d7
Sultan Badri <46205282+SultanBadri@users.noreply.github.com>
2021-02-24 21:34:59 -0800

    added tailwind css, removed boilerplate

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
+import { useState } from "react";
+import Header from "./components/Header";
+import WelcomeModal from "./components/WelcomeModal";
+
 const App: React.FC = () => {
+  const [gameStarted, setGameStarted] = useState(false);
+
+  const startGame = (): void => {
+    setGameStarted(true);
+  };
+
   return (
     <>
-      <h1>hi</h1>
+      <Header />
+      {!gameStarted && <WelcomeModal startGame={startGame} />}
     </>
   );
 };
---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <nav className="bg-green-400 text-white p-6">
+      <h2 className="text-2xl text-white font-bold">Where is Waldo?</h2>
+    </nav>
+  );
+};
+
+export default Header;
